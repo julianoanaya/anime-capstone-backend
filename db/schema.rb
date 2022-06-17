1140497e34517058284795349e72d323284f7800
Julian Anaya <juliananaya531@gmail.com>
2022-06-17 11:59:11 -0700

    made simple seed data

---
+++ db/schema.rb
@@ -0,0 +1,87 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_181334) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "anime_wheres", force: :cascade do |t|
+    t.integer "anime_id"
+    t.integer "where_id"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "animes", force: :cascade do |t|
+    t.string "name"
+    t.integer "seasons"
+    t.integer "episode"
+    t.string "dub"
+    t.string "image_url"
+    t.text "description"
+    t.integer "manga_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.integer "manga_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "manga_anime_genres", force: :cascade do |t|
+    t.integer "manga_id"
+    t.integer "anime_id"
+    t.integer "genre_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "manga_wheres", force: :cascade do |t|
+    t.integer "manga_id"
+    t.integer "where_to_read"
+    t.string "url_manga"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mangas", force: :cascade do |t|
+    t.string "name"
+    t.integer "chapter"
+    t.text "description"
+    t.integer "volume"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "where_to_reads", force: :cascade do |t|
+    t.string "reading_service"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "where_to_watches", force: :cascade do |t|
+    t.string "streaming_service"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Manga.create!(name: "One Piece", chapter: 1053, volume: 102, description: "The series focuses on Monkey D. Luffy, a young man made of rubber, whom, inspired by his childhood idol, the powerful pirate Red-Haired Shanks, sets off on a journey from the East Blue Sea to find the mythical treasure, the One Piece, and proclaim himself the King of the Pirates.")
+Anime.create!(name: "One Piece", seasons: 20, episode: 1014, dub: "Yes", image_url: "www.onepiece.test", description: "The series focuses on Monkey D. Luffy, a young man made of rubber, whom, inspired by his childhood idol, the powerful pirate Red-Haired Shanks, sets off on a journey from the East Blue Sea to find the mythical treasure, the One Piece, and proclaim himself the King of the Pirates.", manga_id: 1)
+WhereToWatch.create!(streaming_service: "crunchy roll")
+Genre.create!(genre: "action")
+WhereToRead.create!(reading_service: "crunchy roll")
+Image.create(url: "www.op_1.test", manga_id: 1)
