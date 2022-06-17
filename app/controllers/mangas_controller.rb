89d6157ebb681fae9f6daa6900eb1c7b4438414e
Julian Anaya <juliananaya531@gmail.com>
2022-06-17 12:21:56 -0700

    made index action

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -1,2 +1,6 @@
 class MangasController < ApplicationController
+  def index
+    mangas= Manga.all
+    render json: mangas.as_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Manga.create!(name: "One Piece", chapter: 1053, volume: 102, description: "The series focuses on Monkey D. Luffy, a young man made of rubber, whom, inspired by his childhood idol, the powerful pirate Red-Haired Shanks, sets off on a journey from the East Blue Sea to find the mythical treasure, the One Piece, and proclaim himself the King of the Pirates.")
+Manga.create!(name: "Naruto", chapter: 700, volume: 72, description: "Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.")
 Anime.create!(name: "One Piece", seasons: 20, episode: 1014, dub: "Yes", image_url: "www.onepiece.test", description: "The series focuses on Monkey D. Luffy, a young man made of rubber, whom, inspired by his childhood idol, the powerful pirate Red-Haired Shanks, sets off on a journey from the East Blue Sea to find the mythical treasure, the One Piece, and proclaim himself the King of the Pirates.", manga_id: 1)
 WhereToWatch.create!(streaming_service: "crunchy roll")
 Genre.create!(genre: "action")
